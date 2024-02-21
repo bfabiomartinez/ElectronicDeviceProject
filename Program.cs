0737d54a091365cf1fb1c7f582f797c84a5e08e2
Bernardo Fabio Menjivar Martinez <110244775+bfabiomartinez@users.noreply.github.com>
2024-02-21 16:05:16 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            IElectronicDevice TV = TVRemote.GetDevice();
+
+            PowerButton powBut = new PowerButton(TV);
+            VolumeButton volBut = new VolumeButton(TV);
+
+            powBut.Execute();
+            powBut.Undo();
+            volBut.Execute();
+            volBut.Undo();
+
+
+        }
+    }
+}
---
+++ VolumeButton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class VolumeButton : ICommand
+    {
+        IElectronicDevice device;
+
+        public VolumeButton(IElectronicDevice device)
+        {
+            this.device = device;
+        }
+        public void Execute()
+        {
+            device.VolumeUp();
+        }
+        public void Undo()
+        {
+            device.VolumeDown();
+        }
+    }
+}
